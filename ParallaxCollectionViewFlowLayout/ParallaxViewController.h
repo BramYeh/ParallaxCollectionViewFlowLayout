5b2f2274ad7cf694b01cf7599ca7bc34d002df6b
Bram (hryeh) <hryeh@yahoo-inc.com>
2014-02-27 02:22:49 +0800

    Add ParallaxCollectionViewFlowLayout

---
+++ ParallaxCollectionViewFlowLayout/ParallaxViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParallaxViewController.h
+//  ParallaxCollectionViewFlowLayout
+//
+//  Created by Bram Yeh on 1/13/14.
+//  Copyright (c) 2014 Bram Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParallaxViewController : UIViewController
+
+@end
---
+++ ParallaxCollectionViewFlowLayout/ParallaxViewController.m
@@ -0,0 +1,29 @@
+//
+//  ParallaxViewController.m
+//  ParallaxCollectionViewFlowLayout
+//
+//  Created by Bram Yeh on 1/13/14.
+//  Copyright (c) 2014 Bram Yeh. All rights reserved.
+//
+
+#import "ParallaxViewController.h"
+
+@interface ParallaxViewController ()
+
+@end
+
+@implementation ParallaxViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
